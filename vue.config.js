d108d45036b3a1163f24664da52bad137b8889e2
liujiarong <godisljr@163.com>
2024-05-19 22:02:28 +0800

    ✨ feat(build): seo perful

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
+const PrerenderSPAPlugin = require("prerender-spa-plugin-next");
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
@@ -7,5 +8,15 @@ module.exports = defineConfig({
         additionalData: `@import "@/assets/css/app.scss";`,
       },
     },
+  },
+  lintOnSave: false,
+  // eslint-disable-next-line no-dupe-keys
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        routes: ["/"], // 需要预渲染的页面，要与router路由一致
+      }),
+    ],
   },
 });
