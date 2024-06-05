08b366b9df2cf8174fab3a81c56f8e51a03b4311
liujiarong <448736378@qq.com>
2024-06-06 02:17:35 +0800

    ✨ feat(all): 资料修改

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@
  * @Author: Liu Jiarong
  * @Date: 2024-04-30 19:25:20
  * @LastEditors: Liu Jiarong
- * @LastEditTime: 2024-05-01 00:19:13
+ * @LastEditTime: 2024-06-06 02:16:05
  * @FilePath: /my-website/src/data/projects.js
  * @Description:
  * @
@@ -39,6 +39,36 @@ const projects = [
     tags: ["vue", "React", "Flutter", "Java", "Node"],
     description:
       "我的 GitLab 代码库涵盖了 JavaScript、Vue、React、Flutter 和 Java 等多种编程语言和框架。在这里，您可以找到搭建好的工程、脚手架以及实用的插件，帮助您快速开启项目开发。无论是前端开发、移动应用开发还是后端开发，您都能找到所需的资源，提升开发效率，轻松构建您的应用程序。 ",
+  },
+  {
+    id: 4,
+    title: "通义万相",
+    image: "Application4.png",
+    url: "https://www.liujiarong.top/wanx-ui",
+    category: "Web网站",
+    tags: ["AI 生成", "图像生成", "文本到图像", "扩散模型", "通义"],
+    description:
+      "体验阿里云通义万相强大的 AI 图像生成能力！通义万相提供丰富的图像生成模型，通过简洁易用的界面，让你轻松将文字转化为精美图片。探索无限创意，开启 AI 图像创作之旅！",
+  },
+  {
+    id: 5,
+    title: "stable-ui",
+    image: "Application5.png",
+    url: "https://www.liujiarong.top/stable-ui/",
+    category: "Web网站",
+    tags: ["AI 绘图", "Stable Diffusion", "Stable Horde", "图像生成", "开源"],
+    description:
+      "🌟 发现新宝藏！🎨🖥️ Hi！给大家分享一个超级棒的AI绘图工具——Stable UI！这个平台可以让你轻松生成、保存和查看由Stable Diffusion技术生成的图片。最棒的是，无论你的设备性能如何，都可以免费使用！ Stable UI利用了Stable Horde，一个由众多Stable Diffusion工作节点组成的分布式集群，确保每个人都能访问到这项先进的AI绘图技术。如果你有多余的GPU算力，还可以加入Stable Horde，为社区做出贡献！  赶快试试吧，期待看到大家的作品！👀💫",
+  },
+  {
+    id: 6,
+    title: "国内大模型",
+    image: "Application6.png",
+    url: "https://chatnio.liujiarong.top",
+    category: "AI应用",
+    tags: ["自然语言处理", "大型语言模型", "中文语义理解", "AI 对话"],
+    description:
+      "体验汇聚国内顶尖技术的 AI 对话平台！国内大模型整合了多家领先的中文大型语言模型，提供流畅自然的对话体验。无论是闲聊、问答还是创作，都能在这里找到智能的伙伴，开启与 AI 无缝沟通的全新方式。",
   },
 ];
 
